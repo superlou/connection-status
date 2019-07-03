17aeaa5241f41a6e8e937310dbb922bbd7297d3a
superlou <lousimons@gmail.com>
2019-07-03 09:20:58 -0400

    Add interface info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use gethostname::gethostname;
+use std::process::Command;
+use std::str;
+use serde::{Serialize, Deserialize};
+use serde_json::{Result};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct IpAddrInterface {
+    address: String,
+    broadcast: String,
+    flags: Vec<String>,
+    link_type: String,
+    operstate: String,
+    addr_info: Vec<AddrInfo>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AddrInfo {
+    broadcast: Option<String>,
+    dynamic: Option<bool>,
+    family: String,
+    label: Option<String>,
+    local: String,
+    noprefixroute: Option<bool>,
+    preferred_life_time: i64,
+    prefixlen: i64,
+    scope: String,
+    valid_life_time: i64,
+}
+
+fn get_ip_info() -> Result<Vec<IpAddrInterface>> {
+    let output = Command::new("ip").args(&["-j", "address"]).output().expect("Failed to execute process");
+    let result = str::from_utf8(&output.stdout).expect("Failed to convert to string");
+    let data: Vec<IpAddrInterface> = serde_json::from_str(result).expect("Failed to parse");
+    return Ok(data)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hostname = gethostname();
+    let hostname = hostname.to_str().unwrap_or("");
+    println!("Hostname: {}", hostname);
+
+    let interfaces = get_ip_info().expect("Failed to find interfaces");
+
+    for interface in interfaces {
+        println!("Link type: {} [{}]", interface.link_type, interface.operstate);
+        for addr_info in interface.addr_info {
+            println!("> {}", addr_info.local);
+        }
+    }
 }
